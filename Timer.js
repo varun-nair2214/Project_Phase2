2887f13c6b267233ae4ce353228af4d46449b4c3
Varun Nair <varun.sreevaram@gmail.com>
2026-04-17 09:46:03 +0530

    TimerSheets
    Added Timer sheets

---
+++ Timer.js
@@ -0,0 +1,5 @@
+const start = document.getElementById("Start");
+const stop = document.getElementById("Stop");
+const reset = document.getElementById("Reset");
+const timer = document.getElementById("Timer");
+
